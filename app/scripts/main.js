d163278338456a08926fc646fa38e4815e15a6ed
Lisa P <passinglisa@gmail.com>
2014-05-05 19:54:00 +0200

    add the map

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,16 @@
-console.log('\'Allo \'Allo!');
+
+$(function() {
+	// create a map in the "map" div, set the view to a given place and zoom
+	var map = L.map('map').setView([52.534400, 13.358581], 15);
+
+	// add an OpenStreetMap tile layer
+	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+	L.tileLayer.provider('OpenStreetMap.HOT').addTo(map);
+
+	// add a marker in the given location, attach some popup content to it and open the popup
+	L.marker([52.534400, 13.358581]).addTo(map);
+		//.bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
+		//.openPopup();
+})
